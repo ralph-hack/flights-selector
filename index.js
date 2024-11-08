9c12b1fabff0e1d8c5d0e966dad2b99984cec899
ralph-hack <76606721+ralph-hack@users.noreply.github.com>
2024-11-08 12:52:37 -0500

    refactor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function processFlights(data) {
+function processFlightsByCityAndPrice(data) {
   const locations = [];
   for (const itinerary of data.destinations) {
     const flight = itinerary.flightInfo;
@@ -58,6 +58,61 @@ function processFlights(data) {
   // return locations;
 }
 
+function processFlights(data, sortStrategy) {
+  const locations = [];
+  for (const itinerary of data.destinations) {
+    const flight = itinerary.flightInfo;
+    const {city, country, airport} = itinerary;
+    const searchUrl = `https://www.kayak.com${itinerary.clickoutUrl}`
+    const location = {
+      destination: {
+        city: {
+            id: city.id,
+            name: city.name
+        },
+        country: {
+            id: country.id, 
+            name: country.name,
+            code: country.code
+        }
+      },
+      label: `${city.name}, ${country.name}`,
+      flight: {
+        expectedPrice: flight.priceUSD,
+        airline: {
+            name: itinerary.airline,
+            code: itinerary.airlineCode
+        },
+        airport: {
+            name: airport.name,
+            code: airport.shortName,
+            latitude: airport.latitude,
+            longitude: airport.longitude
+        }
+      },
+      searchUrl: searchUrl
+    };
+    console.log('location ===> ',location)
+    locations.push(location);
+  }
+
+  locations.sort(sortStrategy);
+  return locations;
+}
+
+const strategyGroupedSortByCityAndPrice = (a, b) => {
+  if (a.destination.city.name !== b.destination.city.name) {
+    //return b.destination.city.name - a.destination.city.name;
+    return a.destination.city.name.localeCompare(b.destination.city.name);
+  } 
+  // else if (a.location.flight.date !== b.location.flight.date) {
+      // return a.flight.date - b.flight.date;
+  // } 
+  else  {
+    return a.flight.expectedPrice - b.flight.expectedPrice;
+  }
+};
+
 // Load the JSON data
 fs.readFile('flights.json', 'utf8', (err, data) => {
   if (err) {
@@ -67,10 +122,8 @@ fs.readFile('flights.json', 'utf8', (err, data) => {
 
   try {
     const json = JSON.parse(data);
-    const groupedData = processFlights(json);
-
-    // Print the grouped data (optional)
-    printGroupedByCity(groupedData);
+    //sortByCityAndPrice(json);
+    sortByCityAndPriceGeneric(json);
 
     // Print list sorted by price
     // printByPrice(groupedData);
@@ -78,13 +131,25 @@ fs.readFile('flights.json', 'utf8', (err, data) => {
     console.error('Error processing JSON data:', error);
   }
 });
+
+function sortByCityAndPriceGeneric(json) {
+  const locations = processFlights(json,strategyGroupedSortByCityAndPrice);
+  const groupedData = groupByCityName(locations);
+  printGroupedByCityAndPrice(groupedData);
+}
+
+function sortByCityAndPrice(json) {
+  const groupedData = processFlightsByCityAndPrice(json);
+  printGroupedByCityAndPrice(groupedData);
+}
+
 function printByPrice(groupedData) {
   for (const location of groupedData) {
     console.log(`${location.label}: $${location.flight.expectedPrice} ${location.searchUrl}`);
   }
 }
 
-function printGroupedByCity(groupedData) {
+function printGroupedByCityAndPrice(groupedData) {
   for (const cityName in groupedData) {
     console.log(`City: ${cityName}`);
     for (const location of groupedData[cityName]) {
